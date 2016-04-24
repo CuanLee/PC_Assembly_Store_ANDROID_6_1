000dc2e777f00782f4f5292fd8833f329c898603
CuanLee <cuanlee27@gmail.com>
2016-04-24 19:06:18 +0200

    Initial RepositoryCommit

---
+++ app/src/main/java/assignment_6/cput/za/ac/pc_assembly_store_app/domain/Employee/Gender.java
@@ -0,0 +1,8 @@
+package assignment_6.cput.za.ac.pc_assembly_store_app.domain.Employee;
+
+/**
+ * Created by Cuan on 4/3/2016.
+ */
+public enum Gender {
+    Male,Female,Other;
+}
---
+++ app/src/main/java/assignment_6/cput/za/ac/pc_assembly_store_app/factories/EmployeeFactory.java
@@ -0,0 +1,16 @@
+package assignment_6.cput.za.ac.pc_assembly_store_app.factories;
+
+import assignment_6.cput.za.ac.pc_assembly_store_app.domain.PC.Chassis;
+import assignment_6.cput.za.ac.pc_assembly_store_app.domain.Employee.ContactDetails;
+import assignment_6.cput.za.ac.pc_assembly_store_app.domain.Employee.Employee;
+import assignment_6.cput.za.ac.pc_assembly_store_app.domain.Employee.Gender;
+import assignment_6.cput.za.ac.pc_assembly_store_app.domain.Employee.GeographicalDetails;
+
+import java.util.List;
+
+/**
+ * Created by Cuan on 4/3/2016.
+ */
+public interface EmployeeFactory {
+    Employee createEmployee(Long id, String username, String password, String firstName, String lastName, String gender, ContactDetails contactDetails, GeographicalDetails geographicalDetails, String idNumber);
+}
